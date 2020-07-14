a7901e805af9f484f32fe973f88a3b6616c083b1
v_cshi <523656402@qq.com>
2020-07-14 19:01:23 +0800

    add net test in aliyun

--- arsenal/memstress/aliyun/index.js
+++ arsenal/memstress/aliyun/index.js
@@ -1,38 +1,38 @@
 const MEGABYTE = 1024 * 1024;
 
 function memIntensive(level) {
-  var available_memory = 512;
-  let amountInMB = available_memory - (available_memory / 10) * (4 - level);
-  console.log(amountInMB);
-  Buffer.alloc(amountInMB * MEGABYTE, 'a');
+    var available_memory = 512;
+    let amountInMB = available_memory - (available_memory / 10) * (4 - level);
+    console.log(amountInMB);
+    Buffer.alloc(amountInMB * MEGABYTE, 'a');
 }
 
 function isWarm() {
-  var is_warm = process.env.warm ? true : false;
-  process.env.warm = true;
-  return is_warm;
+    var is_warm = process.env.warm ? true : false;
+    process.env.warm = true;
+    return is_warm;
 }
 
 function getDuration(startTime) {
-  var end = process.hrtime(startTime);
-  return end[1] + (end[0] * 1e9);
+    var end = process.hrtime(startTime);
+    return end[1] + (end[0] * 1e9);
 }
 
 
 function runTest(intensityLevel){
-  memIntensive(intensityLevel)
+    memIntensive(intensityLevel)
 }
 
 
 
 exports.handler = (req, resp, context) => {
-  var startTime = process.hrtime();
-  runTest(req.queries['level']);
-  var reused = isWarm();
-  var duration = getDuration(startTime);
-  resp.send(JSON.stringify({
-    reused: reused,
-    duration: duration
-  }));
+    var startTime = process.hrtime();
+    runTest(req.queries['level']);
+    var reused = isWarm();
+    var duration = getDuration(startTime);
+    resp.send(JSON.stringify({
+        reused: reused,
+        duration: duration
+    }));
 
 }
---
+++ arsenal/netstress/aliyun/index.js
@@ -0,0 +1,71 @@
+var getRawBody = require('raw-body');
+var getFormBody = require('body/form');
+var body = require('body');
+
+const http = require('http');
+const fs = require('fs');
+
+const files = {1: '/files/1Mb.dat', 2: '/files/10Mb.dat', 3: '/files/100Mb.dat'};
+
+/*
+To enable the initializer feature (https://help.aliyun.com/document_detail/156876.html)
+please implement the initializer function as below：
+exports.initializer = (context, callback) => {
+  console.log('initializing');
+  callback(null, '');
+};
+*/
+async function networkIntensive(level) {
+    const writable = fs.createWriteStream('/dev/null');
+    await new Promise((resolve) => http.get({
+        host: `www.ovh.net`,
+        port: 80,
+        path: files[level]
+    }, (res) => {
+        var download = res.pipe(writable);
+        download.on('close', () => resolve(res));
+    }));
+}
+
+
+function isWarm() {
+    var is_warm = process.env.warm ? true : false;
+    process.env.warm = true;
+    return is_warm;
+}
+
+function getDuration(startTime) {
+    var end = process.hrtime(startTime);
+    return end[1] + (end[0] * 1e9);
+}
+
+function getLevel(event) {
+    let intensityLevel = event.level ? parseInt(event.level) : null;
+    if (!intensityLevel || intensityLevel < 1) {
+        return {"error": "invalid level parameter"};
+    }
+    return intensityLevel;
+}
+
+function getParameters(event) {
+    return getLevel(event);
+}
+
+function runTest(intensityLevel){
+    networkIntensive(intensityLevel);
+}
+
+
+
+exports.handler = async (req, resp, context) => {
+    var startTime = process.hrtime();
+    // console.log(req.queries['level'])
+    runTest(req.queries['level']);
+    var reused = isWarm();
+    var duration = getDuration(startTime);
+    resp.send(JSON.stringify({
+        reused: reused,
+        duration: duration
+    }));
+
+}
